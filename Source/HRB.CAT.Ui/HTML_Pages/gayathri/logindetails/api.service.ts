0b5a5b14386121f4c16c9754efdfa5a17ae8eab6
Nikunjam <gayatri.ajith@gmail.com>
2019-08-20 01:21:11 +0530

    updated login details
    need js-sha512 library

--- Source/HRB.CAT.Ui/HTML_Pages/gayathri/logindetails/api.service.ts
+++ Source/HRB.CAT.Ui/HTML_Pages/gayathri/logindetails/api.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { User } from "./user";
+import { Observable } from "rxjs";
 @Injectable({
   providedIn: "root"
 })
 export class ApiService {
-  apiURL: string = "http://www.server.com/api/";
-
+  apiURL: string =
+    "https://my-json-server.typicode.com/Nikunjam/trialpost/posts";
+  httpOptions = {
+    headers: new HttpHeaders({ contentType: "application/json" })
+  };
   constructor(private httpClient: HttpClient) {}
-
+  // `${this.apiURL}/posts/`
   public sendUserMailID(user: User) {
-    return this.httpClient.post(`${this.apiURL}/login/`, user);
+    return this.httpClient.post(this.apiURL, user);
+  }
+  public getOtphash(): Observable<any> {
+    return this.httpClient.get(this.apiURL);
   }
 }
--- Source/HRB.CAT.Ui/HTML_Pages/gayathri/logindetails/login/login.component.ts
+++ Source/HRB.CAT.Ui/HTML_Pages/gayathri/logindetails/login/login.component.ts
@@ -10,6 +10,7 @@ import {
 } from "@angular/forms";
 import { ErrorStateMatcher } from "@angular/material/core";
 import * as sha512 from "js-sha512";
+import { callNgModuleLifecycle } from "@angular/core/src/view/ng_module";
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(
     control: FormControl | null,
@@ -33,7 +34,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {}
   user = {
-    mail_id: "vishnupotti@hrblock.com"
+    mail_id: ""
   };
   form = new FormGroup({
     emailFormControl: new FormControl("", [
@@ -48,23 +49,35 @@ export class LoginComponent implements OnInit {
   verifyHash() {
     //TODO update the code to compare with the hash obtained by sending email id
     this.trial = "123456";
-    this.val = sha512.sha512_256(this.trial);
-    console.log(this.form.value.emailFormControl);
+    //this.val = sha512.sha512_256(this.trial);
     this.otphash = sha512.sha512_256(this.form.value.emailFormControl);
-    console.log(this.val);
-    console.log(this.otphash);
-    if (this.val !== this.otphash) {
-      alert("not same");
-    }
-  }
-  sendMailID() {
-    this.otp = true;
     console.log(this.form.value.emailFormControl);
-    this.apiService.sendUserMailID(this.form.value).subscribe(response => {
-      console.log("Sent a mail id");
-      //console.log(response.json());
+    this.apiService.getOtphash().subscribe(data => {
+      console.log(data);
+      this.val = data.otphash;
+      console.log(this.val);
+      console.log(this.otphash);
+      if (this.val != this.otphash) {
+        alert("not same");
+      } else {
+        alert("Thank god");
+      }
     });
   }
+  res: any;
+  sendMailID() {
+    this.otp = true;
+    //console.log(this.form.value.emailFormControl);
+
+    this.user.mail_id = this.form.value.emailFormControl;
+    this.apiService.sendUserMailID(this.user).subscribe(response => {
+      console.log("Sent a mail id");
+      console.log(response);
+      this.res = response;
+      console.log(this.res);
+    });
+  }
+
   otp = false;
   expanded = false;
   matcher = new MyErrorStateMatcher();
